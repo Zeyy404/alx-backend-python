0f9e22f4f64a2c4838cff06b09d40f35bb5b2351
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-15 23:12:06 +0400

    Task 10{advanced}

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """A module to Augment the code with a duck-typed annotations"""
-from typing import Any, List, Optional
+from typing import Any, Sequence, Union
 
 
-def safe_first_element(lst: List[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any]:
     """returns the first element of a list"""
     if lst:
         return lst[0]
