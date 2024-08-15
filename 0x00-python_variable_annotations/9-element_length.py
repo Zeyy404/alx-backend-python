5becebb206ef670754b3b786ddd89d7110f0e5ce
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-15 18:41:52 +0400

    Task 9{modified}

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-A module to annotate a give function’s parameters and
+A module to annotate a given function’s parameters and
 return values with the appropriate types
 """
-from typing import Iterable, Sequence, Tuple, List
+from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
