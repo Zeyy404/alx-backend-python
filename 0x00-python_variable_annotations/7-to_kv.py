127754a06080d61e5846e7cda9860fc0941eb502
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-15 18:30:53 +0400

    Tasks 7 - 9

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function `to_kv` that takes a string `k` and
+an int OR float `v` as arguments and returns a tuple of string `k` &
+square of the int/float `v` annotated as a float.
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns a tuple of string `k` &
+    square of the int/float `v` annotated as a float.
+    """
+    return (k, float(v * v))
---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A module to annotate a give function’s parameters and
+return values with the appropriate types
+"""
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
