24ad6f8d50b0b8861e5c5b7ab7c6cc0bf844b78a
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-20 20:52:00 +0400

    Tasks 0 - 2

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A coroutine called `async_generator` that takes no arguments module"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    A coroutine that loops 10 times,
+    each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
